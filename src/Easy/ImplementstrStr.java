51d96b38fa6eec949d21b7581f651d2068d73917
Sagar Mangulkar <sagarmangulkar01@gmail.com>
2024-09-08 08:40:44 -0500

    Solve - Implement strStr

---
+++ src/Easy/ImplementstrStr.java
@@ -0,0 +1,47 @@
+/*
+
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+ 
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+ 
+
+Constraints:
+
+1 <= haystack.length, needle.length <= 104
+haystack and needle consist of only lowercase English characters.
+
+*/
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (haystack.length() < needle.length()) {
+            return -1;
+        }
+        int i = 0, j = 0;
+        while (j < haystack.length() ) {
+            if (haystack.charAt(j) == needle.charAt(i)) {
+                if (i == needle.length() - 1) {
+                    return j - i;
+                }
+                i++;
+                j++;
+            } else {
+                j = j - i + 1;
+                i = 0;
+            }
+        }
+        return -1;
+    }
+}
