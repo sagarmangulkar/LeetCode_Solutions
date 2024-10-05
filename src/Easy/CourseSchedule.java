c8725a9f0fbc3eaf5f30c904e9886da64d95669d
Sagar Mangulkar <sagarmangulkar01@gmail.com>
2024-10-05 18:01:24 -0500

    Solve - Course Schedule

---
+++ src/Easy/CourseSchedule.java
@@ -0,0 +1,64 @@
+/*
+
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+Return true if you can finish all courses. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: true
+Explanation: There are a total of 2 courses to take. 
+To take course 1 you should have finished course 0. So it is possible.
+Example 2:
+
+Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take. 
+To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+ 
+
+Constraints:
+
+1 <= numCourses <= 2000
+0 <= prerequisites.length <= 5000
+prerequisites[i].length == 2
+0 <= ai, bi < numCourses
+All the pairs prerequisites[i] are unique.
+
+*/
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> nodesWithEdges = new ArrayList<>(numCourses);
+        for (int i = 0; i < numCourses; i++) {
+            nodesWithEdges.add(new ArrayList<>());
+        }
+        int[] inDegrees = new int[numCourses];
+        for (int[] prerequisite : prerequisites) {
+            nodesWithEdges.get(prerequisite[1]).add(prerequisite[0]);
+            inDegrees[prerequisite[0]]++;
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < inDegrees.length; i++) {
+            if (inDegrees[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        int countVisits = 0;
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            countVisits++;
+            for (int neighbor : nodesWithEdges.get(node)) {
+                inDegrees[neighbor]--;
+                if (inDegrees[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+        return countVisits == numCourses;
+    }
+}
