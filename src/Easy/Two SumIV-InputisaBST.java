25e74f2c056bdfe829aeb7efe4e81041917967d0
Sagar Mangulkar <sagarmangulkar01@gmail.com>
2025-02-25 20:17:57 -0600

    Solve - Two SumIV - Input is a BST

---
+++ src/Easy/Two SumIV-InputisaBST.java
@@ -0,0 +1,59 @@
+/*
+
+Given the root of a binary search tree and an integer k, return true if there exist two elements in the BST such that their sum is equal to k, or false otherwise.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,3,6,2,4,null,7], k = 9
+Output: true
+Example 2:
+
+
+Input: root = [5,3,6,2,4,null,7], k = 28
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-104 <= Node.val <= 104
+root is guaranteed to be a valid binary search tree.
+-105 <= k <= 105
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        Set<Integer> set = new HashSet<>();
+        return helper(root, k, set);
+    }
+
+    public boolean helper(TreeNode root, int k, Set<Integer> set) {
+        if (root == null) return false;
+        if (!set.contains(root.val)) {
+            set.add(k - root.val);
+        }
+        else {
+            return true;
+        }
+        return helper(root.left, k, set) || helper(root.right, k, set);
+    }
+}
