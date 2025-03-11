1d4e6e582c1429ef5864f6f844e210f080914c9d
Sagar Mangulkar <sagarmangulkar01@gmail.com>
2025-03-11 14:13:56 -0500

    Solve with Priority Queue

--- src/Easy/MeetingScheduler.java
+++ src/Easy/MeetingScheduler.java
@@ -60,3 +60,29 @@ class Solution {
         return output;
     }
 }
+
+// Solve with priority queue
+class Solution2 {
+    public List<Integer> minAvailableDuration(int[][] slots1, int[][] slots2, int duration) {
+        PriorityQueue<int[]> timeslots = new PriorityQueue<>(
+            (slot1, slot2) -> slot1[0] - slot2[0]
+        );
+
+        for(int[] slot : slots1)
+            if(slot[1] - slot[0] >= duration)
+                timeslots.offer(slot);
+
+        for(int[] slot : slots2)
+            if(slot[1] - slot[0] >= duration)
+                timeslots.offer(slot);
+
+        while(timeslots.size() > 1) {
+            int[] slot1 = timeslots.poll();
+            int[] slot2 = timeslots.peek();
+            if(slot1[1] >= slot2[0] + duration)
+                return new ArrayList<>(Arrays.asList(slot2[0], slot2[0] + duration));
+        }
+
+        return new ArrayList<>();
+    }
+}
