6a46e5ff8c6806efdce1a1fabd1535502b2d33d5
Sagar Mangulkar <sagarmangulkar01@gmail.com>
2025-03-13 16:38:13 -0500

    Solve - Maximal Square using DP

--- src/Easy/MaximalSquare.java
+++ src/Easy/MaximalSquare.java
@@ -70,3 +70,26 @@ class Solution {
         return squareLen;
     }
 }
+
+
+
+
+//DP
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int row = matrix.length;
+        int col = row > 0 ? matrix[0].length : 0;;
+        int maxSquareLen = 0;
+        int[][] dp = new int[row+1][col+1];
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                if (matrix[i-1][j-1] == '0') {
+                    continue;
+                }
+                dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                maxSquareLen = Math.max(maxSquareLen, dp[i][j]);
+            }
+        }
+        return maxSquareLen * maxSquareLen;
+    }
+}
