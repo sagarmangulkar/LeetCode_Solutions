a794d7758ea0b615e13944266df3baeef6646175
Sagar Mangulkar <sagarmangulkar01@gmail.com>
2025-04-07 21:23:22 -0500

    Solve - Last Stone Weight using PriorityQueue

--- src/Easy/LastStoneWeight.java
+++ src/Easy/LastStoneWeight.java
@@ -51,3 +51,23 @@ class Solution {
         return stones[0];
     }
 }
+
+//using PriorityQueue
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            (a, b) -> b-a
+        );
+        for (int i = 0; i < stones.length; i++) {
+            pq.add(stones[i]);
+        }
+        while (pq.size() > 1) {
+            int y = pq.poll();
+            int x = pq.poll();
+            if (x < y) {
+                pq.add(y-x);
+            }
+        }
+        return (pq.isEmpty()) ? 0 : pq.poll();
+    }
+}
