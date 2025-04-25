42f2382b57dd1dee8e10d0e1d3acf1e4718e3fa0
Sagar Mangulkar <sagarmangulkar01@gmail.com>
2025-04-25 00:32:02 -0500

    Solve - Delete and Earn using DP Top Down Memoization

---
+++ src/Easy/DeleteandEarn.java
@@ -0,0 +1,62 @@
+/*
+
+You are given an integer array nums. You want to maximize the number of points you get by performing the following operation any number of times:
+
+Pick any nums[i] and delete it to earn nums[i] points. Afterwards, you must delete every element equal to nums[i] - 1 and every element equal to nums[i] + 1.
+Return the maximum number of points you can earn by applying the above operation some number of times.
+
+ 
+
+Example 1:
+
+Input: nums = [3,4,2]
+Output: 6
+Explanation: You can perform the following operations:
+- Delete 4 to earn 4 points. Consequently, 3 is also deleted. nums = [2].
+- Delete 2 to earn 2 points. nums = [].
+You earn a total of 6 points.
+Example 2:
+
+Input: nums = [2,2,3,3,3,4]
+Output: 9
+Explanation: You can perform the following operations:
+- Delete a 3 to earn 3 points. All 2's and 4's are also deleted. nums = [3,3].
+- Delete a 3 again to earn 3 points. nums = [3].
+- Delete a 3 once more to earn 3 points. nums = [].
+You earn a total of 9 points.
+ 
+
+Constraints:
+
+1 <= nums.length <= 2 * 104
+1 <= nums[i] <= 104
+
+*/
+
+// DP Top Down Memoization
+
+class Solution {
+    Map<Integer, Integer> memo = new HashMap<>();
+    Map<Integer, Integer> totalVal = new HashMap<>();
+    public int deleteAndEarn(int[] nums) {
+        int maxNum = Integer.MIN_VALUE;
+        for (int num : nums) {
+            totalVal.put(num, totalVal.getOrDefault(num, 0) + num);
+            maxNum = Math.max(maxNum, num);
+        }
+        return dp(maxNum);
+    }
+
+    public int dp(int i) {
+        if (i == 0) {
+            return 0;
+        }
+        if (i == 1) {
+            return totalVal.getOrDefault(1, 0);
+        }
+        if (!memo.containsKey(i)) {
+            memo.put(i, Math.max(dp(i-1), dp(i-2) + totalVal.getOrDefault(i, 0)));
+        }
+        return memo.get(i);
+    }
+}
