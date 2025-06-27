a829010cb9b46c4dba0678a4b2416565096990d7
Sagar Mangulkar <sagarmangulkar01@gmail.com>
2025-06-26 23:51:09 -0500

    Solve - DotProduct of Two Sparse Vectors

---
+++ src/Easy/DotProductofTwoSparseVectors.java
@@ -0,0 +1,65 @@
+/*
+
+Given two sparse vectors, compute their dot product.
+
+Implement class SparseVector:
+
+SparseVector(nums) Initializes the object with the vector nums
+dotProduct(vec) Compute the dot product between the instance of SparseVector and vec
+A sparse vector is a vector that has mostly zero values, you should store the sparse vector efficiently and compute the dot product between two SparseVector.
+
+Follow up: What if only one of the vectors is sparse?
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,0,0,2,3], nums2 = [0,3,0,4,0]
+Output: 8
+Explanation: v1 = SparseVector(nums1) , v2 = SparseVector(nums2)
+v1.dotProduct(v2) = 1*0 + 0*3 + 0*0 + 2*4 + 3*0 = 8
+Example 2:
+
+Input: nums1 = [0,1,0,0,0], nums2 = [0,0,0,0,2]
+Output: 0
+Explanation: v1 = SparseVector(nums1) , v2 = SparseVector(nums2)
+v1.dotProduct(v2) = 0*0 + 1*0 + 0*0 + 0*0 + 0*2 = 0
+Example 3:
+
+Input: nums1 = [0,1,0,0,2,0,0], nums2 = [1,0,0,0,3,0,4]
+Output: 6
+ 
+
+Constraints:
+
+n == nums1.length == nums2.length
+1 <= n <= 10^5
+0 <= nums1[i], nums2[i] <= 100
+
+*/
+
+class SparseVector {
+    int[] nums;
+    SparseVector(int[] nums) {
+        this.nums = nums;
+    }
+    
+	// Return the dotProduct of two sparse vectors
+    public int dotProduct(SparseVector vec) {
+        int thisN = this.nums.length;
+        int vecN = vec.nums.length;
+        if (thisN != vecN) {
+            return -1;
+        }
+        int sum = 0;
+        for (int i = 0; i < this.nums.length; i++) {
+            sum = sum + (this.nums[i] * vec.nums[i]);
+        }
+        return sum;
+    }
+}
+
+// Your SparseVector object will be instantiated and called as such:
+// SparseVector v1 = new SparseVector(nums1);
+// SparseVector v2 = new SparseVector(nums2);
+// int ans = v1.dotProduct(v2);
