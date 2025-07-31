997aae5df1273d9b7d8814ee45217d77c10065e3
Sagar Mangulkar <sagarmangulkar01@gmail.com>
2025-07-30 23:44:09 -0500

    Solve - Merge In Between Linked Lists using multiple while stops

--- src/Easy/MergeInBetweenLinkedLists.java
+++ src/Easy/MergeInBetweenLinkedLists.java
@@ -76,3 +76,35 @@ class Solution {
         return list1;
     }
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution2 {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+        ListNode curr = list1;
+        int i = 0;
+        while (i < a-1) {
+            curr = curr.next;
+            i++;
+        }
+        ListNode list1NextToB = curr.next;
+        while (i < b) {
+            list1NextToB = list1NextToB.next;
+            i++;
+        }
+        curr.next = list2;
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        curr.next = list1NextToB;
+        return list1;
+    }
+}
