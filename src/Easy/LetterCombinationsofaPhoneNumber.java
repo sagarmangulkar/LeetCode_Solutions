157e0a30ccae0cf0e4da3a45c5aee8e3b5cfd944
Sagar Mangulkar <sagarmangulkar01@gmail.com>
2025-08-15 00:12:26 -0500

    Solve - Letter Combinations of a Phone Number

---
+++ src/Easy/LetterCombinationsofaPhoneNumber.java
@@ -0,0 +1,61 @@
+/*
+
+Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+
+A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+
+ 
+
+Example 1:
+
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+Example 2:
+
+Input: digits = ""
+Output: []
+Example 3:
+
+Input: digits = "2"
+Output: ["a","b","c"]
+ 
+
+Constraints:
+
+0 <= digits.length <= 4
+digits[i] is a digit in the range ['2', '9'].
+
+*/
+
+class Solution {
+    Map<Character, List<Character>> map = new HashMap<>();
+    public List<String> letterCombinations(String digits) {
+        List<String> ans = new ArrayList<>();
+        if (digits == null || digits.length() == 0) {
+            return ans;
+        }
+        map.put('2', Arrays.asList('a', 'b', 'c'));
+        map.put('3', Arrays.asList('d', 'e', 'f'));
+        map.put('4', Arrays.asList('g', 'h', 'i'));
+        map.put('5', Arrays.asList('j', 'k', 'l'));
+        map.put('6', Arrays.asList('m', 'n', 'o'));
+        map.put('7', Arrays.asList('p', 'q', 'r', 's'));
+        map.put('8', Arrays.asList('t', 'u', 'v'));
+        map.put('9', Arrays.asList('w', 'x', 'y', 'z'));
+        helper(digits, new StringBuilder(), ans, 0);
+        return ans;
+    }
+
+    private void helper(String digits, StringBuilder sb, List<String> ans, int pos) {
+        if (sb.length() == digits.length()) {
+            ans.add(sb.toString());
+            return;
+        }
+        for (char letterChar : map.get(digits.charAt(pos))) {
+            sb.append(letterChar);
+            helper(digits, sb, ans, pos+1);
+            sb.deleteCharAt(sb.length()-1);
+        }
+    }
+}
