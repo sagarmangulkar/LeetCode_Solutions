38c7f5e52e8f3aa94744e9e674f48c1072895186
Sagar Mangulkar <sagarmangulkar01@gmail.com>
2025-08-25 23:08:13 -0500

    Solve - Find Words That Can Be Formed by Characters

---
+++ src/Easy/FindWordsThatCanBeFormedbyCharacters.java
@@ -0,0 +1,53 @@
+/*
+
+You are given an array of strings words and a string chars.
+
+A string is good if it can be formed by characters from chars (each character can only be used once for each word in words).
+
+Return the sum of lengths of all good strings in words.
+
+ 
+
+Example 1:
+
+Input: words = ["cat","bt","hat","tree"], chars = "atach"
+Output: 6
+Explanation: The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
+Example 2:
+
+Input: words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+Output: 10
+Explanation: The strings that can be formed are "hello" and "world" so the answer is 5 + 5 = 10.
+ 
+
+Constraints:
+
+1 <= words.length <= 1000
+1 <= words[i].length, chars.length <= 100
+words[i] and chars consist of lowercase English letters.
+
+*/
+
+class Solution {
+    public int countCharacters(String[] words, String chars) {
+        int sum = 0;
+        int[] freqChar = new int[26];
+        for (char ch : chars.toCharArray()) {
+            freqChar[ch - 'a']++;
+        }
+        for (String word : words) {
+            int[] freqCharProcessing = Arrays.copyOf(freqChar, 26);
+            for (int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+                if (freqCharProcessing[ch - 'a'] <= 0) {
+                    break;
+                }
+                freqCharProcessing[ch - 'a']--;
+                if (i == word.length() - 1) {
+                    sum += word.length();
+                }
+            }
+        }
+        return sum;
+    }
+}
