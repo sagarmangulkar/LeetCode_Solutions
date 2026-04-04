3269934b44809b4dfff22a281f07daf5d00facf6
Sagar Mangulkar <sagarmangulkar01@gmail.com>
2026-04-04 00:06:24 -0500

    Tried one more time

--- src/Easy/DesignCircularQueue.java
+++ src/Easy/DesignCircularQueue.java
@@ -140,3 +140,83 @@ class MyCircularQueue {
  * boolean param_5 = obj.isEmpty();
  * boolean param_6 = obj.isFull();
  */
+
+//-----------------------------
+//Tried one more time
+class MyCircularQueue {
+    int[] arr;
+    int head;
+    int tail;
+    int capacity;
+    int size;
+
+    public MyCircularQueue(int k) {
+        capacity = k;
+        arr = new int[capacity];
+        head = -1;
+        tail = -1;
+        size = 0;
+    }
+    
+    public boolean enQueue(int value) {
+        if (isFull()) {
+            return false;
+        }
+        tail = (tail >= capacity - 1) ? 0 : tail + 1;
+        arr[tail] = value;
+        size++;
+        //special case just for the first time
+        if (head == -1) {
+            head++;
+        }
+        return true;
+    }
+    
+    public boolean deQueue() {
+        if (isEmpty()) {
+            return false;
+        }
+        head = (head >= capacity - 1) ? 0 : head + 1;
+        size--;
+        return true;
+    }
+    
+    public int Front() {
+        if(isEmpty()) {
+            return -1;
+        }
+        return arr[head];
+    }
+    
+    public int Rear() {
+        if(isEmpty()) {
+            return -1;
+        }
+        return arr[tail];
+    }
+    
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean isFull() {
+        if (size == capacity) {
+            return true;
+        }
+        return false;
+    }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
